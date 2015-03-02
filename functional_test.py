077a4386ec18766281a3db557e7b2292f0d2b7dd
Dirk De Hertog <ddehertog@Dirks-MacBook-Pro.local>
2015-03-02 22:42:46 +0100

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 __author__ = 'ddehertog'
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,25 +19,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # and is expecting to see a to do list app
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
         # you can add items in the textbox
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
+
+        # she wants to go and buy peakock feathers
+        inputbox.send_keys("Buy peacock feathers")
 
         # pressing enter they are stored
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
 
         # the user sees this as the page is automatically updated and is immediately invited to do another one
-
-
         # each time you add one and press enter this is visible
-
+        self.fail("Finish the test!")
 
         # The user expects the items to be stored. A unique url makes sure they are retrievable
 
-
         # she visits that url - her list is still there
-
 
         # then she quietly walks away and leaves
 
