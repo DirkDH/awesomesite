3b2b27d0ce5df079aa4a0b14662589f949a5fb48
Dirk De Hertog <ddehertog@Dirks-MacBook-Pro.local>
2015-03-03 15:41:55 +0100

    before refactoring

--- functional_test.py
+++ functional_test.py
@@ -37,16 +37,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows)
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # the user sees this as the page is automatically updated and is immediately invited to do another one
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         # each time you add one and press enter this is visible
-        self.fail("Finish the test!")
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1 : Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2 : Use peacock feathers to make a fly', [row.text for row in rows])
+
 
         # The user expects the items to be stored. A unique url makes sure they are retrievable
+        self.fail("Finish the test!")
 
         # she visits that url - her list is still there
 
