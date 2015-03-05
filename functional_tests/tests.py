bd983b5db15652abd924184c43b371860da3e222
Dirk De Hertog <ddehertog@Dirks-MacBook-Pro.local>
2015-03-05 08:38:50 +0100

    make functional tests an app, make it a LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,12 @@
 __author__ = 'ddehertog'
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,9 +15,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # someone checks out the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # and is expecting to see a to do list app
         self.assertIn('To-Do', self.browser.title)
@@ -35,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # pressing enter they are stored
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1 : Buy peacock feathers')
 
         # the user sees this as the page is automatically updated and is immediately invited to do another one
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -48,8 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         # each time you add one and press enter this is visible
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1 : Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2 : Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1 : Buy peacock feathers')
+        self.check_for_row_in_list_table('2 : Use peacock feathers to make a fly')
 
 
         # The user expects the items to be stored. A unique url makes sure they are retrievable
@@ -58,6 +61,3 @@ class NewVisitorTest(unittest.TestCase):
         # she visits that url - her list is still there
 
         # then she quietly walks away and leaves
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
