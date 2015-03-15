5f98776ebbafb37c08ac9d8b17fa528832eab2b1
Dirk De Hertog <ddehertog@Dirks-MacBook-Pro.local>
2015-03-15 14:39:33 +0100

    rename allm input ids and names.still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
 
 
         # you can add items in the textbox
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'), 'Enter a to-do item'
         )
@@ -36,7 +36,7 @@ class NewVisitorTest(FunctionalTest):
         self.check_for_row_in_list_table('1 : Buy peacock feathers')
 
         # the user sees this as the page is automatically updated and is immediately invited to do another one
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -60,7 +60,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('make a fly', page_text)
 
         # Francis enters a new item
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
